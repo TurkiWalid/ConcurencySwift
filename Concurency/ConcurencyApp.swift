b012d0fe4bb56e853449321e3e3c8fe8d655b99a
walid <walid.turki.94@gmail.com>
2023-05-23 19:32:53 +0100

    Initial Commit

---
+++ Concurency/ConcurencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurencyApp.swift
+//  Concurency
+//
+//  Created by Walid TURKI on 23/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct ConcurencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
